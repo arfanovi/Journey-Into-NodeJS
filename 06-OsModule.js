822a85adcd7e98f05b1ff143c626e5c2444de66a
arfanovi <arfanovi.gub1@gmail.com>
2024-07-08 18:15:21 +0600

    OS Module

---
+++ 06-OsModule.js
@@ -0,0 +1,13 @@
+
+
+
+const os = require("os");
+console.log(os.arch());
+console.log(os.cpus());
+console.log(os.freemem());
+console.log(os.networkInterfaces());
+
+
+console.log(os.hostname());
+console.log(os.homedir());
+console.log(os.userInfo())
