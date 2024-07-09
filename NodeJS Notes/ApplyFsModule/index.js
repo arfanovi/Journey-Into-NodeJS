55d220fd86bcd6ebbf149a05403dea94563aa0e5
arfanovi <arfanovi.gub1@gmail.com>
2024-07-09 18:56:52 +0600

    Apply Async with fs module read/write

---
+++ NodeJS Notes/ApplyFsModule/index.js
@@ -0,0 +1,27 @@
+let fs = require('fs');
+let http = require('http');
+
+let server = http.createServer(function (req, res) {
+    if (req.url === "/") {
+        // Async read
+        fs.readFile('Home.html', function (err, data) {
+            if (err) {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                res.write('<h1>File Not Found</h1>');
+                res.end();
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                res.end();
+            }
+        });
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.write('<h1>404 Not Found</h1>');
+        res.end();
+    }
+});
+
+server.listen(1000, () => {
+    console.log('Server is running on port 1000');
+});
